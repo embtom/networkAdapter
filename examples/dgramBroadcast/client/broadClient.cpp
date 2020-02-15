05cb264e0a50daad686acdf9b349891437806c85
tom <t.willetal@googlemail.com>
2020-02-15 19:31:55 +0100

    start with dgramExample

---
+++ examples/dgramBroadcast/client/broadClient.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <BaseSocket.hpp>
+#include <dgram/DgramClient.hpp>
+#include <span.h>
+
+
+#define PORT_NUM 50002
+
+
+int main(int argc, char *argv[])
+{
+    char buffer [128];
+    utils::span<char> rxSpan(buffer);
+
+    auto baseSocket = EtNet::CBaseSocket(EtNet::ESocketMode::INET_DGRAM);
+    EtNet::CDgramClient dgramClient(std::move(baseSocket));
+
+    auto[a, b] = dgramClient.getLink("localhost",PORT_NUM);
+
+    for (int i = 0 ; i < 2; i++)
+    {
+        std::string test = std::string("Hallo") + std::to_string(i);
+        a.send(utils::span<char>(test));
+            a.recive(rxSpan, [](utils::span<char> rx) {
+            std::cout << "Rcv Len: " << rx.data() << " Size: " << rx.size() << std::endl;
+            return true;
+        });
+    }
+}
---
+++ examples/dgramBroadcast/server/broadServer.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <dgram/DgramServer.hpp>
+#include <thread>
+#include <span.h>
+
+
+#define PORT_NUM 50002
+
+int main()
+{
+    auto baseSocket = EtNet::CBaseSocket(EtNet::ESocketMode::INET_DGRAM);
+    EtNet::CDgramServer DgramServer(std::move(baseSocket), PORT_NUM);
+
+    while (true)
+    {
+        char buffer [128] = {0};
+        utils::span<char> rxtxSpan (buffer);
+
+        EtNet::CDgramDataLink a;
+        std::tie(a) = DgramServer.waitForConnection();
+        a.reciveFrom(rxtxSpan,[&a, &DgramServer](EtNet::SPeerAddr ClientAddr, utils::span<char> rx)
+        {
+            std::cout << "Message form: " << ClientAddr.Ip.toString() << " with length: " << rx.size() << std::endl;
+
+            for(auto &elem : rx) {
+                elem = toupper(elem);
+            }
+
+            a.sendTo(ClientAddr, rx);
+            return true;
+        });
+
+    }
+}
+
